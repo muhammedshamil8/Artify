f6b3e516e5c615bb7be9a81463802a039f87409e
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-11-16 05:05:24 +0530

    feat: initial project setup with configs and structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-2xl mt-10 font-bold text-center'>
+        Hello Vite + React!
+      </h1>
+    </>
+  )
+}
+
+export default App
