eb8939ca5b6eeaded8ee72a0bea94e6bcd60a14d
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-11-17 19:55:48 +0530

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
+import Image from '@/assets/images/page1.png'
+import Card from '@/assets/images/Card.svg'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-2xl mt-10 font-bold text-center'>
-        Hello Vite + React!
-      </h1>
+      <div className='flex max-w-[600px] mx-auto justify-center flex-col'>
+        <img src={Image} alt='page1' />
+        <img src={Card} alt='card' />
+      </div>
     </>
   )
 }
