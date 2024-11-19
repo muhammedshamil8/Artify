86fe02cb6d0ae4e53a5b6506ad3b9aa351c6e5e2
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-11-20 00:30:00 +0530

    chore: update package.json and refactor App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home, IndividualResult, LeaderBoard } from './pages';
 import Layout from '@/components/Layout';  // Import the Layout component
 import '@/assets/styles/index.css';
+import { motion, AnimatePresence } from "motion/react"
 
 function App() {
   return (
     <Router>
       <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/LeaderBoard" element={<LeaderBoard />} />
-          <Route path="/IndividualResult" element={<IndividualResult />} />
-        </Routes>
+        <AnimatePresence>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/LeaderBoard" element={<LeaderBoard />} />
+            <Route path="/IndividualResult" element={<IndividualResult />} />
+          </Routes>
+        </AnimatePresence>
       </Layout>
     </Router>
   );
--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,6 +2,14 @@ import Star from './Star.svg';
 import ArtifyTxt from './Artify_2024.svg';
 import VstarRB from './VectorStar2.svg';
 import VstarLB from './VectorStar.svg';
+import VstarT from './VectorStarTop.svg';
+import VstarLT from './VectorStar3.svg';
+import VstarRT from './VectorStarTop2.svg';
+import PosterStar from './PosterStar.svg'
+import GroupCard from './group_card.png';
+import ResultCard from './Result_card.png';
+import Poster from './poster.png';
 
 
-export { Star, ArtifyTxt , VstarRB , VstarLB };
+
+export { Star, ArtifyTxt, VstarRB, VstarLB, VstarT, VstarRT, VstarLT, PosterStar, ResultCard, GroupCard , Poster };
