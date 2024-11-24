64349bbe3895b7ea00290cae03bafcba2949af85
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-11-25 00:41:30 +0530

    feat(ui): enhance ResultPoster component for better visuals

--- src/components/Ui/ResultPoster/index.jsx
+++ src/components/Ui/ResultPoster/index.jsx
@@ -32,26 +32,37 @@ function dynamicTextWithBreak(text) {
 }
 
 
-function index() {
+function index({ result }) {
+    console.log(result)
+
+    // To log details of winners as well:
+    console.log(result?.winners);
 
     const data = [
         {
-            name: 'John Doe avarachan',
+            name: 'Muhammed Shamil ',
             position: 1
         },
         {
-            name: 'Jane Doe NalLaath',
+            name: 'Saleel Mohammed',
             position: 2
         },
         {
-            name: 'John Doe',
+            name: 'Dilshad KT',
             position: 3
         }
     ];
 
+    const getYear = (year) => {
+        if (year === 1) return '(1st year}';
+        if (year === 2) return '(2nd year)';
+        if (year === 3) return '(3rd year)';
+        if (year === 4) return '(4th year)';
+        return '';
+    }
 
     return (
-        <div className='max-w-[550px] mx-auto border py-4 rounded-md flex flex-col w-full gap-6'>
+        <div className='max-w-[400px] mx-auto border py-4 rounded-md flex flex-col w-full gap-6 overflow-hidden'>
             {/* banner */}
             <div className='bg-[#513B25]  h-10 my-2  w-full overflow-hidden z-20'>
                 <div className='marquee flex justify-start h-full gap-4'>
@@ -73,25 +84,26 @@ function index() {
             </div>
 
             <div className='flex  items-center justify-center w-fit mx-auto'>
-                <img src={ResultTxt} alt='text' className='mx-auto' />
-                <span className='bg-[#D39E2B] rounded-full -ml-8 mt-4 p-0.5 font-bold w-[38px] h-[38px] flex items-center justify-center text-sm text-white'>001</span>
+                <img src={ResultTxt} alt='text' className='mx-auto max-w-[190px]' />
+                <span className='bg-[#D39E2B] rounded-full -ml-8 mt-4 p-0.5 font-bold w-[35px] h-[35px] flex items-center justify-center text-sm text-white'>001</span>
             </div>
 
             {/* result  card*/}
             <div className='px-4 w-full mx-auto'>
                 <div
-                    className={`bg-[#513B25] rounded-xl relative  max-h-[500px] min-w-[300px] max-w-[550px] w-full  mx-auto`}
+                    className={`bg-[#513B25] rounded-xl relative min-w-[300px] max-w-[320px] w-full  mx-auto`}
                 >
                     <div
                         className="absolute top-0 left-0 w-full h-full z-0"
                         style={{
                             backgroundImage: `url(${PosterBG})`,
                             backgroundRepeat: 'repeat',
-                            backgroundSize: 'auto 120px', // Control height of each image and maintain original width, create a gap by controlling height
+                            backgroundSize: 'auto 200px', // Increased height for bigger images
                             backgroundPosition: '0 0',
-                            opacity: 0.1, // Adjust this value to control the opacity
+                            opacity: 0.1, // Slightly increased opacity for visibility
                         }}
                     />
+
 
                     {/* Background Image */}
                     {/* <img src={PosterBG} alt="poster" className=" opacity-10 absolute top-0 left-0 z-0" />
@@ -101,27 +113,30 @@ function index() {
                     <div className="relative z-10 flex flex-col p-4 w-full">
                         <div className="flex flex-col justify-center w-full items-center gap-6">
                             <div>
-                                <h1 className='font-bold text-2xl text-white'> Speech English</h1>
+                                <h1 className='font-bold text-2xl text-white uppercase'>{result?.programName}</h1>
                             </div>
                             {/* <div className="basis-[20%] relative break-words flex items-center justify-center w-20 z-20 text-white">
                             {dynamicTextWithBreak('Speech English')}
                         </div> */}
                             <div className="max-w-full w-fit mx-auto">
-                                {data.map((item, index) => (
+                                {result?.winners && result?.winners.map((item, index) => (
                                     <div className="flex items-start justify-start gap-6 mb-4 w-full" key={index}>
                                         {/* Badge */}
                                         <div className="relative flex items-center justify-center h-10">
                                             <img src={badge} className="h-10" alt="badge" />
-                                            <span className="absolute font-bold text-xl text-white p-1">{item.position}</span>
+                                            <span className="absolute font-bold text-xl text-white p-1">{item?.position}</span>
                                         </div>
                                         {/* Name and Details */}
                                         <div className='flex flex-col flex-1 flex-grow'>
-                                            <div className="flex flex-col">
-                                                <span className="font-bold text-lg leading-4 mt-1 capitalize text-[#FFBF34]">
-                                                    {item.name}
-                                                </span>
-                                                <span className="text-xs capitalize text-white">{item.name}</span>
-                                            </div>
+                                            {item.participants.map((item, index) => (
+                                                <div className="flex flex-col">
+                                                    <span className="font-bold text-lg leading-4 mt-1 capitalize text-[#FFBF34] max-w-[240px]">
+                                                        {item?.name}
+                                                    </span>
+                                                    <span className="text-xs capitalize text-white">{item?.department} {getYear(item?.year)}</span>
+                                                </div>
+                                            ))}
+
                                             {/* <div className="flex flex-col">
                                             <span className="font-bold text-lg leading-4 mt-1 capitalize text-[#FFBF34]">
                                                 {item.name}
@@ -145,17 +160,18 @@ function index() {
                         <img src={wideStar} alt='star' className='mx-auto pr-10' />
                     </div>
                 </div>
+                <div className='flex items-center justify-between w-full max-w-[320px] mx-auto mt-10'>
+                    <div>
+                        <img src={poster_element} alt='element' className='mx-auto' />
+                    </div>
+                    <div>
+                        <img src={UnionLogo} alt='logo' className='mx-auto' />
+                    </div>
+
+                </div>
             </div>
             {/* element */}
-            <div className='flex items-center justify-around w-full'>
-                <div>
-                    <img src={poster_element} alt='element' className='mx-auto' />
-                </div>
-                <div>
-                    <img src={UnionLogo} alt='star' className='mx-auto' />
-                </div>
 
-            </div>
         </div>
     )
 }
